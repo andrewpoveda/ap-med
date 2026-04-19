9ad1ce1c2df0aebe28452aab3d0aae5898817c3b
Andrew Poveda <apmedpodcast@gmail.com>
2026-04-19 18:35:09 -0400

    commit

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -10,7 +10,8 @@ export default function Navigation() {
   const { theme } = useTheme();
 
   const navItems = [
-    { href: "/", label: "about" },
+    { href: "/", label: "home" },
+    { href: "/about", label: "about" },
     { href: "/projects", label: "projects" },
     { href: "/blog", label: "blog" },
     { href: "/mentors", label: "mentors" }
